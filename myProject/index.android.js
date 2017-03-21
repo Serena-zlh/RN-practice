a67a8c95da791cc9ff9f16d26c826b07a6a8a059
zhenglinghua <zhenglinghua@zhenglinghuadeMacBook-Pro.local>
2017-03-21 20:28:35 +0800

    路由

--- myProject/index.android.js
+++ myProject/index.android.js
@@ -6,48 +6,52 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+		AppRegistry,
+		StyleSheet,
+		Text,
+		View,
+		Navigator
 } from 'react-native';
 
+import Footer from './component/Footer.android.js'
+
 export default class myProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+	render() {
+		let defaultPage = Footer;
+		return (
+		        < View >
+				< Navigator
+				initialRoute = {
+					{ name: '首页', component: defaultPage } }
+				configureScene = {
+					(route) => {
+						return Navigator.SceneConfigs.FloatFromRight; } }
+				renderScene = {
+					(route, navigator) => {
+						let Component = route.component;
+							return <Component {...route.params } navigator = { navigator }/> }} / >
+				< /View>
+						);
+				}
+		}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+		container: {
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: '#F5FCFF',
+		},
+		welcome: {
+				fontSize: 20,
+				textAlign: 'center',
+				margin: 10,
+		},
+		instructions: {
+				textAlign: 'center',
+				color: '#333333',
+				marginBottom: 5,
+		},
 });
 
 AppRegistry.registerComponent('myProject', () => myProject);
